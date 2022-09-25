dc0528cc1017184acffcb55f0e2a72170fbb06eb
tibet <tibetakinci@gmail.com>
2022-09-25 13:31:04 +0200

    barrier script implementation v4

---
+++ Assets/Scripts/ButtonBarrierController.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Numerics;
+using UnityEngine;
+
+public class ButtonBarrierController : MonoBehaviour
+{
+    private UnityEngine.Vector3 startPosition;
+    private float targetYPosition = -0.6f;
+    public float buttonSpeed;
+    private bool isPressed = false;
+    private bool isUp = true;
+    private bool isClicked = false;
+
+    public GameObject barrier;
+    public float barrierSpeed;
+    private float barrierRotationAmount = 270f;
+    private float closedBarrierRotationAmount = 359f;
+    private bool isOpen = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isPressed)
+        {
+            if(isClicked)
+                AnimateButton();
+            AnimateBarrier();
+        }
+    }
+
+    void OnMouseDown()
+    {
+        isPressed = true;
+        isClicked = true;
+    }
+
+    void AnimateBarrier()
+    {
+        if(!isOpen)
+        {
+            OpenBarrier();
+        }
+        if(barrierRotationAmount >= barrier.transform.rotation.eulerAngles.z)
+        {
+            isOpen = true;
+        }
+        if(isOpen)
+        {
+            CloseBarrier();
+        }
+        if(closedBarrierRotationAmount <= barrier.transform.rotation.eulerAngles.z)
+        {
+            isOpen = false;
+            isPressed = false;
+        }
+    }
+
+    void OpenBarrier()
+    {
+        barrier.transform.Rotate(UnityEngine.Vector3.back * barrierSpeed * Time.deltaTime);
+    }
+
+    void CloseBarrier()
+    {
+        barrier.transform.Rotate(UnityEngine.Vector3.forward * barrierSpeed * Time.deltaTime);
+    }
+
+    void AnimateButton()
+    {
+        if(isUp)
+        {
+            ButtonPushDown();
+        }
+        if(transform.position.y <= targetYPosition)
+        { 
+            isUp = false;
+        }
+        if(!isUp) 
+        {
+            ButtonPullUp();
+        }
+        if(transform.position.y >= startPosition.y)
+        {
+            isUp = true;
+            isClicked = false;
+        }
+    }
+
+    void ButtonPushDown()
+    {
+        transform.position -= transform.forward * buttonSpeed * Time.deltaTime;
+    }
+
+    void ButtonPullUp()
+    {
+        transform.position += transform.forward * buttonSpeed * Time.deltaTime;
+    }
+}
