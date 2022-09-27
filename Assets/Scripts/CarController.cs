751fdc8f656a009acde1069c2633dfc578b4a449
tibet <tibetakinci@gmail.com>
2022-09-27 19:44:44 +0200

    car movement lerp function implementation v4

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -5,11 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class CarController : MonoBehaviour
 {   
-    private int TotalLeftCarAmount = 5;
-    private int TotalRightCarAmount = 4;
-    private int LeftCarCount = 1;
-    private int RightCarCount = 1;
-
     public float speed = 10.0f;
     private Rigidbody rb;
     private GameObject tick;
@@ -18,14 +13,14 @@ public class CarController : MonoBehaviour
     public Transform gridTransform;
     private float t = 0.1f;
     private ButtonBarrierController bbController;
-    public bool isOpen;
-    public bool firstPosition = false;
     private Vector3 firstLeftCarPosition = new Vector3(-12.8f, 4.4f, 31.5f);
     private Vector3 secondLeftCarPosition = new Vector3(-12.8f, 4.4f, 42f);
     private Vector3 firstRightCarPosition = new Vector3(13.2f, 4.4f, 31.5f);
     private Vector3 secondRightCarPosition = new Vector3(13.2f, 4.4f, 42f);
     private Quaternion secondCarRotation = new Quaternion(0f, 270f, 180f, 0f);
-    public bool isMoving = false;
+    public bool isOpen;
+    public bool isFirst;
+    public bool isMoving;
     private bool isInstantiate;
 
     // Start is called before the first frame update
@@ -34,32 +29,17 @@ public class CarController : MonoBehaviour
         rb = this.GetComponent<Rigidbody>();
         tick = this.transform.Find("tick").gameObject;
         isInstantiate = false;
+        isFirst = false;
+        isMoving = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(isInstantiate)
-            if(this.gameObject.tag == "Left Car" && LeftCarCount < TotalLeftCarAmount)
-            {
-                Instantiate(this, secondLeftCarPosition, secondCarRotation);
-                LeftCarCount++;
-            }
-            else if(this.gameObject.tag == "Right Car" && RightCarCount < TotalRightCarAmount)
-            { 
-                Instantiate(this, secondRightCarPosition, secondCarRotation);
-                RightCarCount++;
-            }
-            isInstantiate = false;
-        
-        if((CheckZPosition(firstRightCarPosition.z) || CheckZPosition(firstLeftCarPosition.z)) && !isOpen)
-            isInstantiate = true;
-            firstPosition = true;
-        
         isOpen = bbController.isOpen;
         if(isOpen || isMoving)
         {
-            if (firstPosition)
+            if (isFirst)
             {
                 MoveTowards(gridTransform.position);
                 isMoving = CheckPosition(gridTransform.position);
@@ -72,7 +52,7 @@ public class CarController : MonoBehaviour
                     isMoving = CheckPosition(firstLeftCarPosition);
                     /*
                     if(!isOpen && CheckFirstPosition(firstLeftCarPosition))
-                        firstPosition = true;
+                        isFirst = true;
                         //Instantiate(this.gameObject, secondLeftCarPosition, secondCarRotation);
                         */
                 }
@@ -84,6 +64,23 @@ public class CarController : MonoBehaviour
                     if(!isMoving)
                         Instantiate(this, secondRightCarPosition, secondCarRotation);
                          */
+                }
+            }
+        }
+
+        if((CheckZPosition(firstRightCarPosition.z) || CheckZPosition(firstLeftCarPosition.z)) && !isOpen)
+        {
+            isFirst = true;
+            if(!isInstantiate)
+            {
+                isInstantiate = true;
+                if(this.gameObject.tag == "Left Car")
+                {
+                    Instantiate(this.gameObject, secondLeftCarPosition, secondCarRotation);
+                }
+                else if(this.gameObject.tag == "Right Car")
+                { 
+                    Instantiate(this, secondRightCarPosition, secondCarRotation);
                 }
             }
         }
@@ -102,14 +99,12 @@ public class CarController : MonoBehaviour
 
     bool CheckZPosition(float z)
     {
-        UnityEngine.Debug.Log(Mathf.Abs(transform.position.z - z));
         return Mathf.Abs(transform.position.z - z) <= 0.1f;
     }
 
     bool CheckPosition(Vector3 endPosition)
     {
-        UnityEngine.Debug.Log(Vector3.Distance(transform.position, endPosition));
-        return Vector3.Distance(transform.position, endPosition) >= 0.1f;
+        return Vector3.Distance(this.transform.position, endPosition) >= 0.7f;
     }
 
     void OnCollisionEnter(Collision collision)
@@ -122,11 +117,6 @@ public class CarController : MonoBehaviour
         else if(collision.gameObject.tag == "Line")
         {
             UnityEngine.Debug.Log("Line");
-            rb.velocity = new UnityEngine.Vector3(0f, 0f, 0f);
-        }
-        else if(collision.gameObject.tag == "Left Grid" || collision.gameObject.tag == "Right Grid")
-        {
-            UnityEngine.Debug.Log("Grid");
         }
     }
 
